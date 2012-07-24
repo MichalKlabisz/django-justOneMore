2324540fd4ba15184279bb5a81a37b5f56bcada0
Michal Klabisz <mk_cafe9@o2.pl>
2012-07-24 08:08:54 +0200

    Preparing views and templates

--- website/views.py
+++ website/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+
+from django.views.generic.list import ListView
+from website.models import Image
+
+class HomeView(ListView):
+
+    context_object_name = 'images'
+    model = Image
+    queryset = Image.objects.all()
+    template_name = 'base.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(HomeView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['selected_page'] = 'home'
+        
+        return context
