119c8b2ee3562a687ba4dfa709dd3d321ccc865b
root <root@puppy.(none)>
2012-07-30 11:51:24 +0200

    Working on details view and layout

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.list import ListView
 from website.models import Image
 from django.views.generic.detail import DetailView
-from django.views.generic.base import TemplateView
-from website.views import NewestView, Top100View, UploadView, LoginView
+from website.views import NewestView, Top100View, UploadView
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,11 +14,12 @@ urlpatterns = patterns('',
     #                                        model=Image
     #                                         )
     #    ),
-    #url(r'^(?P<slug>[-a-zA-Z0-9]+)/$', DetailView.as_view(
-    #                                        template_name='detail.html',
-    #                                        model=Image
-    #                                         )
-    #    )
+    url(r'^img/(?P<slug>[-a-zA-Z0-9]+)/$', DetailView.as_view(
+                                            template_name='details.html',
+                                            context_object_name='image',
+                                            model=Image
+                                             )
+        ),
     url(r'^$', NewestView.as_view()),
     url(r'^top100/$', Top100View.as_view()),
     url(r'^upload/$', UploadView.as_view()),
